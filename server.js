fc22009947a09d444c06b320a4740d25d7de8ff8
theDreamer911 <thetrulydreamer@gmail.com>
2020-09-14 23:03:10 +0700

    file upload database filepond 2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    limit: '10mb',
+    limit: '50mb',
     extended: false
 }))
 
